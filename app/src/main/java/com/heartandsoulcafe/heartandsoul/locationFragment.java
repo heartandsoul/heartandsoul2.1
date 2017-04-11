1a740d8e53987a496e9bdfa1eb7c1f2e77d2713e
Mohan <mohan_lmc2010@yahoo.com>
2017-04-11 21:08:24 +1000

    basic code for next sprint

---
+++ app/src/main/java/com/heartandsoulcafe/heartandsoul/locationFragment.java
@@ -0,0 +1,50 @@
+package com.heartandsoulcafe.heartandsoul;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class locationFragment extends Fragment implements OnMapReadyCallback {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_location, container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment fragment = SupportMapFragment.newInstance();
+        getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+
+
+
+       LatLng marker = new LatLng(12, 151);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 20));
+
+        googleMap.addMarker(new MarkerOptions().title("Heart and soul location!").position(marker));
+    }
+
+
+}
